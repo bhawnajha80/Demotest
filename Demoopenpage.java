24d8b2019d665f17aa211dc29efd8903f0f0d985
bhawnajha80 <jha.bhawna@icloud.com>
2020-06-05 00:16:21 -0400

    commit first git demo

--- Demoopenpage.java
+++ Demoopenpage.java
@@ -1,17 +1,20 @@
 package GithubDemo;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class Demoopenpage {
 
 	public static WebDriver driver;
 	
-	@BeforeClass
+	@Test
 	public void Pageopen() {
 		
-		driver = new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver", "C:\\Bhawna\\Java Selenium links\\FireFoxDriver\\geckodriver.exe");
+		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
 		
 		driver.get("http://www.newtours.demoaut.com/");
